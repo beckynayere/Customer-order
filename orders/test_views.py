ec106071957e8c7c8210e1941467cd0cad71ed91
beckynayere <rebecca.nayere@gmail.com>
2024-11-20 13:04:45 +0300

    create a github workflow

--- orders/test_views.py
+++ orders/test_views.py
@@ -1,18 +1,10 @@
-from django.test import TestCase
-
-# Create your tests here.
-
 from rest_framework.test import APITestCase
-from django.urls import reverse
-from .models import Customer, Order
 from rest_framework import status
+from .models import Customer, Order
 from .africastalking_config import sms
 
-class CustomerOrderTests(APITestCase):
+class CustomerOrderTestCase(APITestCase):
 
-    def setUp(self):
-        self.customer_data = {"name": "John Doe", "code": "C123"}
-        self.order_data = {"item": "Laptop", "amount": 1000, "time": "2024-11-19T12:00:00", "customer": 1}
     def setUp(self):
         # Create a customer for testing
         self.customer = Customer.objects.create(name="John Doe", code="C123", phone_number="+254711123456")
@@ -21,21 +13,11 @@ class CustomerOrderTests(APITestCase):
             "item": "Laptop",
             "amount": 1200.50
         }
-    def test_add_customer(self):
-        response = self.client.post(reverse('add-customer'), self.customer_data)
-        self.assertEqual(response.status_code, 201)
 
-    def test_add_order(self):
-        customer = Customer.objects.create(**self.customer_data)
-        self.order_data['customer'] = customer.id
-        response = self.client.post(reverse('add-order'), self.order_data)
-        self.assertEqual(response.status_code, 201)
-        
     def test_create_order(self):
         # Test creating an order
         response = self.client.post("/api/orders/", self.order_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
 
     def test_sms_sent_on_order(self):
         # Mock the SMS sending function
